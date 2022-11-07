facb8916c90545a000a23be34300408910cac6d5
spinlud <ludovico.fabbri@gmail.com>
2022-11-07 12:36:37 +0100

    Renew certificate

--- certbot/renew-certificate.sh
+++ certbot/renew-certificate.sh
@@ -9,4 +9,4 @@ sudo docker run -it --rm --name certbot \
     -v "/etc/letsencrypt:/etc/letsencrypt" \
     -v "/var/lib/letsencrypt:/var/lib/letsencrypt" \
     -v "/var/www/letsencrypt:/var/www/letsencrypt" \
-    certbot/certbot certonly --webroot -w /var/www/letsencrypt --agree-tos -m $EMAIL -d $DOMAIN --dry-run
+    certbot/certbot certonly --webroot -w /var/www/letsencrypt --agree-tos -m $EMAIL -d $DOMAIN # Add --dry-run to simulate
