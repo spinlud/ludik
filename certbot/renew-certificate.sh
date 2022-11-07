b7ec39037ae09bfadf1d09b72df490d32d57de6b
spinlud <ludovico.fabbri@gmail.com>
2022-11-07 12:23:14 +0100

    Renew certificate

--- certbot/renew-certificate.sh
+++ certbot/renew-certificate.sh
@@ -8,6 +8,5 @@ EMAIL="ludovico.fabbri@gmail.com"
 sudo docker run -it --rm --name certbot \
     -v "/etc/letsencrypt:/etc/letsencrypt" \
     -v "/var/lib/letsencrypt:/var/lib/letsencrypt" \
-    -p 80:80 \
-    -p 443:443 \
-    certbot/certbot certonly --force-renew --standalone --non-interactive --agree-tos -m $EMAIL -d $DOMAIN
+    -v "/var/www/letsencrypt:/var/www/letsencrypt" \
+    certbot certonly --webroot -w /var/www/letsencrypt --agree-tos -m $EMAIL -d $DOMAIN --dry-run
